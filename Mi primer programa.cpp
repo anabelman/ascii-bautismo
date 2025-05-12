dd62f32b3d5670ef0602173278e83c39b2ca3f40
Ana Belman <annabelmaaan@gmail.com>
2025-05-11 21:38:04 -0600

    Create Mi primer programa.cpp

---
+++ Mi primer programa.cpp
@@ -0,0 +1,20 @@
+// Este programa me ayudará a practicar C++
+#include <iostream>
+
+int main() {
+   // Fun Fact: Se me dificulta hacer esto.
+
+  // Mi programa vive aqui
+
+  std::cout << "   A     B B B B  \n";
+  std::cout << "  A A    B      B \n";
+  std::cout << " A   A   B      B \n";
+  std::cout << "A A A A  B B B B  \n";
+  std::cout << "A     A  B      B \n";
+  std::cout << "A     A  B      B \n";
+  std::cout << "A     A  B B B B  \n";
+
+return 0;
+// Este es el final de mi programa.
+}
+
